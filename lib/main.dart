5c56ee28aa278d503ac17e5e41f31806e1217bf1
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2025-01-21 19:17:24 -0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '/pages/home.dart';
+import '/pages/choose_location.dart';
+import '/pages/loading_screen.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
