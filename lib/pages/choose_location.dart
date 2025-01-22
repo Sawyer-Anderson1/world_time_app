acd844d3d09a104c2930aa72c75dc11c8520f59b
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2025-01-21 19:21:45 -0600

    Add files via upload

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -10,6 +10,13 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Choose a Location",
+        ),
+      ),
+
+    );
   }
 }
--- lib/pages/loading_screen.dart
+++ lib/pages/loading_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Text("Loading Screen"),
+    );
   }
 }
+
 
